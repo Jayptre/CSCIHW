3e46af2616785cfe643c363dabd1b4d160b472ce
jayptre <petrykij@go.stockton.edu>
2021-10-09 11:52:41 -0400

    HW Updates

---
+++ HW2/Triangle.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+public class Triangle {
+
+    public static String Pythagorean() {
+
+        Scanner sc = new Scanner(System.in);
+        double side1, side2, hypotenuse;
+        System.out.println("Welcome to the Pythagorean Theorem Calculator!");
+        System.out.println("Please Enter A Value For Side 1: ");
+        side1 = sc.nextDouble();
+        System.out.println("Please Enter A Value for Side 2: ");
+        side2 = sc.nextDouble();
+        hypotenuse = Math.sqrt((side1*side1)+(side2*side2));
+        System.out.println("The length of the Hypotenuse is: " + hypotenuse);
+
+        
+        return "Quick Maths";
+    }
+    
+}
