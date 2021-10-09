a63ade995bf9eac490bde3e852196eb20d3cf975
jayptre <petrykij@go.stockton.edu>
2021-10-09 10:51:42 -0400

    updates

---
+++ HW2/MathGame.java
@@ -0,0 +1,13 @@
+public class MathGame {
+
+    public static void main(String[] args) {
+
+        System.out.println("HW2: Begin!");
+        //Pythagorean Theorem Object
+        {
+            String trianglePythagorean = Triangle.Pythagorean();
+            System.out.println(trianglePythagorean);
+        }
+    }
+    
+}
