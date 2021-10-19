53d716361cd0224f9741dfebcfad267e6ade8926
jayptre <petrykij@go.stockton.edu>
2021-10-19 19:36:07 -0400

    notes

---
+++ HW2/Tips.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tips {
+
+    public static String Bill() {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    double people, bill, percent;
+    System.out.println("Welcome to the Tip Calculator!");
+    System.out.println("Please ");
+
+    }
+}
